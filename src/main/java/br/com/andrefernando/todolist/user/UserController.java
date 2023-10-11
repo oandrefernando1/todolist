c605bf09fcc8b8f3ebcc6ee686bb04e728868292
Andre <andrefernandolara@hotmail.com>
2023-10-10 22:32:21 -0300

    Aula 2

--- src/main/java/br/com/andrefernando/todolist/user/UserController.java
+++ src/main/java/br/com/andrefernando/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.andrefernando.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
   
-
+    @Autowired
+    private IUserRepository userRepository;
   /**
    * String (texto)
    * Integet (int) numeros inteiros
@@ -29,7 +33,16 @@ public class UserController {
    * Body 
    */
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel){
-      System.out.println(userModel.name);
-  } 
+  public ResponseEntity create(@RequestBody UserModel userModel){
+      var user = this.userRepository.findByUsername(userModel.getUsername());
+
+      if(user != null){
+        // Mensagem de erro
+        // Status Code
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe!");
+      }
+
+      var  userCreated = this.userRepository.save(userModel);
+      return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+  }
 }
